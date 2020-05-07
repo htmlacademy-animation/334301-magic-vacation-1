ea9cf4953c0c3c5490a49d4e54c792e6e6d65868
Keks <keks@htmlacademy.ru>
2020-05-07 13:29:18 +0300

    :cat:

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -1,10 +1,4 @@
 import throttle from 'lodash/throttle';
-import game from './game';
-import prizes from './prizes';
-
-const STORY_SCREEN_ID = 1;
-const PRIZES_SCREEN_ID = 2;
-const GAME_SCREEN_ID = 4;
 
 export default class FullPageScroll {
   constructor() {
@@ -14,17 +8,15 @@ export default class FullPageScroll {
     this.menuElements = document.querySelectorAll(`.page-header__menu .js-menu-link`);
 
     this.activeScreen = 0;
-    this.previousScreen = 0;
     this.onScrollHandler = this.onScroll.bind(this);
-    this.onUrlHashChengedHandler = this.onUrlHashChenged.bind(this);
+    this.onUrlHashChengedHandler = this.onUrlHashChanged.bind(this);
   }
 
   init() {
-    document.addEventListener(`wheel`, throttle(this.onScrollHandler, this.THROTTLE_TIMEOUT));
+    document.addEventListener(`wheel`, throttle(this.onScrollHandler, this.THROTTLE_TIMEOUT, {trailing: true}));
     window.addEventListener(`popstate`, this.onUrlHashChengedHandler);
 
-    this.onUrlHashChenged();
-    this.changePageDisplay();
+    this.onUrlHashChanged();
   }
 
   onScroll(evt) {
@@ -35,7 +27,7 @@ export default class FullPageScroll {
     }
   }
 
-  onUrlHashChenged() {
+  onUrlHashChanged() {
     const newIndex = Array.from(this.screenElements).findIndex((screen) => location.hash.slice(1) === screen.id);
     this.activeScreen = (newIndex < 0) ? 0 : newIndex;
     this.changePageDisplay();
@@ -48,46 +40,10 @@ export default class FullPageScroll {
   }
 
   changeVisibilityDisplay() {
-    const previousScreen = Array.from(this.screenElements).findIndex((screen) => screen.classList.contains(`active`));
-
-    if (previousScreen === STORY_SCREEN_ID && this.activeScreen === PRIZES_SCREEN_ID) {
-      this.screenElements[previousScreen].querySelector(`.screen__background`).classList.add(`screen__background--scaled`);
-
-      setTimeout(() => {
-        this.screenElements.forEach((screen) => {
-          screen.classList.add(`screen--hidden`);
-          screen.classList.remove(`active`);
-        });
-
-        this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
-        this.screenElements[this.activeScreen].classList.add(`active`);
-        prizes.initiatePrizes();
-      }, 1000);
-
-      return;
-    }
-
-    if (this.activeScreen === PRIZES_SCREEN_ID && previousScreen !== STORY_SCREEN_ID) {
-      prizes.initiatePrizes();
-    } else {
-      prizes.deactivatePrizes();
-    }
-
-    this.screenElements.forEach((screen, index) => {
+    this.screenElements.forEach((screen) => {
       screen.classList.add(`screen--hidden`);
       screen.classList.remove(`active`);
-
-      if (index === STORY_SCREEN_ID) {
-        this.screenElements[index].querySelector(`.screen__background`).classList.remove(`screen__background--scaled`);
-      }
     });
-
-    if (this.activeScreen === GAME_SCREEN_ID) {
-      game.runCounterAnimation();
-    } else {
-      game.stopCounterAnimation();
-    }
-
     this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
     this.screenElements[this.activeScreen].classList.add(`active`);
   }
